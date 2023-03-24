f7fcc8a9466433bd3a8cc51f2d00a0290adba8c0
Lorenzo Bonatti <lorenzo.bonatti@3ds.team>
2023-03-24 10:26:42 +0100

    fix: add undefined specification to SidebarProps for children. Update version v0.2.1-alpha

--- src/lib/Sidebar.tsx
+++ src/lib/Sidebar.tsx
@@ -68,7 +68,7 @@ export interface SidebarProps {
      * @default undefined
      */
     onClose?: () => void;
-    children: ReactElement;
+    children?: ReactElement;
 }
 
 export default function Sidebar(
